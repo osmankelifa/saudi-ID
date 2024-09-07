7f466d347dde93e775b414272f84509060df72a6
sheriff702 <ahmed.sharif@hyperisland.se>
2024-09-07 21:21:33 +0200

    chore: Remove FeaturesList component from Home page
    The FeaturesList component was commented out in order to improve the page load performance. This change is part of optimizing the Home page and ensuring a better user experience.

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       <About />
       {/* <NumberBlock /> */}
       <Testimonials />
-      <FeaturesList />
+      {/* <FeaturesList /> */}
       <NewsSection />
       <LogoArea />
 
